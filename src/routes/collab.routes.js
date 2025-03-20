44aeded334c6305805d3815921cfdcdb50dcf570
ASCodeAlchemy <ayushsonar04@gmail.com>
2025-03-20 14:33:29 +0530

    refactor: update reward verification and claiming logic in collaboration routes

--- src/routes/collab.routes.js
+++ src/routes/collab.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { registerCollaborator, loginCollaborator } from "../controller/collab.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { verifyReward } from "../controller/rewards.controller.js";
-
+import { verifyReward } from "../controller/collab.controller.js";
+import { claimReward } from "../controller/rewards.controller.js";
 const router = Router();
 
 router.route('/register').post(registerCollaborator);
 router.route('/login').post(loginCollaborator);
 router.route('/verify').post(verifyJWT, verifyReward);
-router.route('/claim').post(verifyJWT, verifyReward, (req, res) => {
+router.route('/claim').post(verifyJWT, claimReward, (req, res) => {
    
     res.status(200).json(new ApiResponse(200, req.reward, "Reward claimed successfully"));
 });
